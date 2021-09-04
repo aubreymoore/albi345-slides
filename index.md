fbdfafb77027cb9ffeb7956ee8815d74c5cb84ef
aubreymoore <aubreymoore@guam.net>
2021-09-04 19:47:19 +1000

    /home/aubrey/ALBI-345/files/pdfs/internal-morphology.pdf

--- index.md
+++
@@ -1,2 +0,0 @@
-[Orders with Complete Metamorphosis](https://aubreymoore.github.io/albi345-slides/Orders-with-Complete-Metamorphosis)
-
--- pdf2reveal.sh
+++ pdf2reveal.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "$1"
-if test -f "$1"; then
+echo $1
+if test -r $1
+then
     papermill -p PDFPATH $1 pdf2reveal.ipynb out.ipynb
+	git pull
+	git add .
+	git commit -m "$1"
+	git push
+else
+    echo "ERROR: Cannot find PDF at $1"
 fi
